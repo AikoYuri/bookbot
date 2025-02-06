19a708dbfe260083f3a35e12f34cd3b51d990eef
aiko <monsteeh@gmail.com>
2025-02-06 01:10:53 +0100

    Added colors and exception management

--- main.py
+++ main.py
@@ -1,12 +1,35 @@
 def main():
-    with open("./books/frankenstein.txt") as f:
-        file_contents = f.read()
-    
+    while True:
+        try:
+            path = input(f"\nInsert the path to a txt file:\nOr press {bcolors.OKBLUE}CTRL+C{bcolors.ENDC} to quit\n")
+            with open(path) as f:
+                file_contents = f.read()
+                break
+        except Exception as e:
+            print (f"\n{bcolors.FAIL}Wrong filepath/extension:{bcolors.ENDC}\n{e}\n")
+            pass
     wline()
     print(file_contents)
     wline()
     report(count_words(file_contents), count_char(file_contents))
-    
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+
+    def disable(self):
+        self.HEADER = ''
+        self.OKBLUE = ''
+        self.OKGREEN = ''
+        self.WARNING = ''
+        self.FAIL = ''
+        self.ENDC = ''
+
+   
 def count_words(text_in):
     x = text_in.split()
     return len(x)
@@ -22,15 +45,16 @@ def count_char(text_in):
     return char_count
 
 def report(words_count, char_count):
-    print (f"{words_count} words found in the document")
+    print (f"{bcolors.OKGREEN}{words_count}{bcolors.ENDC} words found in the document")
     for k in char_count:
         if k.isalpha() != True:
             continue
-        print (f"The '{k}' character was found {char_count[k]} times")
+        print (f"The {bcolors.OKBLUE}'{k}'{bcolors.ENDC} character was found {bcolors.OKGREEN}{char_count[k]}{bcolors.ENDC} times")
 
 def wline():
     print("\n\n")
     for i in range (0,3):
         print("*******************************************")
-    print("\n\n")         
+    print("\n\n")
+             
 main()
